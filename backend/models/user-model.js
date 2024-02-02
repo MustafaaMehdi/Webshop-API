573685178f4e43604bc5ca258b69964525181e98
MustafaaMehdi <anfsmus@gmail.com>
2024-02-03 00:38:03 +0100

    :sparkles: feat(users): Initialized user methods

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -5,3 +5,5 @@ const UserSchema = mongoose.Schema({
     email: String,
     password: String
 })
+
+module.exports = mongoose.model('user', UserSchema)
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const UserModel = require('../models/user-model');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('WELCOME to the GET method in users route');
+router.get('/', async (req, res) => {
+  const users = await UserModel.find().select("-password");
+  res.status(200).json(users);
+});
+
+router.post('/', async (req, res) => {
+  const id = req.body.id;
+  const user = await UserModel.findById(id); //kolla handledning sen om OK
+  res.status(200).json(user);
+});
+
+router.post('/add', async (req, res) => {
+  const user = await UserModel.create(req.body);
+  res.status(201).json(user);
+});
+
+router.post('/login', async (req, res) => {
+  // const inputEmail = req.body.email
+  // const inputPassword = req.body.password
+  const user = await UserModel.findOne({email: req.body.email, password: req.body.password});
+  res.status(201).json(user);
 });
 
 module.exports = router;
