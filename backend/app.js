b1717bb19e39ce853d61e1f51984ccf54fbf64e0
MustafaaMehdi <anfsmus@gmail.com>
2024-02-02 23:10:27 +0100

    :wrench: chore: set-up schema

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/products');
+var ordersRouter = require('./routes/orders');
 
 var app = express();
 
@@ -14,7 +17,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+async function init() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/mustafa-mehdi')    
+        console.log('Database is up and running');
+    } catch (error) {
+        console.log('Database error', error);
+    }
+}
+
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/products', productsRouter);
+app.use('/api/orders', ordersRouter);
+
 
 module.exports = app;
+
+init();
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('WELCOME to the GET method in users route');
 });
 
 module.exports = router;
