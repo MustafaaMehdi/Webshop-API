bb71347b7d5b631baac421aaf20a0922abf31c1c
MustafaaMehdi <anfsmus@gmail.com>
2024-02-03 20:22:32 +0100

    :sparkles: feat(orders): Initialized orders methods

---
+++ backend/models/order-model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'user'
+    },
+    products: [
+        {
+            productId: {
+                type: mongoose.Types.ObjectId,
+                ref: 'product'
+            },
+            quantity: {
+                type: Number,
+                default: 1 
+            }
+        }
+    ]
+});
+
+module.exports = mongoose.model('order', OrderSchema);
--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+// const { ObjectId } = require('mongodb');
+// const mongoose = require('mongoose');
+const OrderModel = require('../models/order-model')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/all', async (req,res) => {
+  const order = await OrderModel.find()
+  res.status(201).json(order);
+});
+
+router.post('/add', async (req,res) => {
+    const order = await OrderModel.create({
+      user: req.body.user,
+      products: req.body.products,
+    });
+
+    const placedOrder = await order.save();
+    console.log({user: order.name});
+    res.status(201).json(placedOrder);
 });
 
 module.exports = router;
